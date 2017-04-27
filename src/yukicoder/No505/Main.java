0019cb970024e74c1ea4401429ac1c95aac6bf9f
Itsuki Aoyagi <mail@yagi2.com>
2017-04-27 17:30:06 +0900

    WA

--- src/yukicoder/No505/Main.java
+++ src/yukicoder/No505/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+//TODO WA
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
